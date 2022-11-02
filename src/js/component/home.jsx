63d9bb5d318a91017156e119d7b68221aaa2473b
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2022-11-02 13:16:59 +0100

    first commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import FormularioTareas from "./FormularioTareas.jsx";
+
+//create your first component
+const Home = () => {
+	const [tareas, setListaTareas] = useState([]);
+
+	const agregarTarea = (nombreTarea) => {
+		const auxTarea = tareas.concat(nombreTarea);
+		setListaTareas(auxTarea);
+	};
+	const eliminarTarea = (index) => {
+		const auxTarea = tareas.filter((tarea, auxIndex) => {
+			if (index !== auxIndex) {
+				return tarea;
+			}
+		});
+		setListaTareas(auxTarea);
+	};
+
+	return (
+		<div className="container">
+
+			<h1>Lista de Tareas</h1>
+			<FormularioTareas tarea={agregarTarea} />
+			{tareas.map((tarea, index) => {
+				return (
+					<>
+						<h6>
+
+							{tarea}
+							<button
+								id="eliminar"
+								onClick={() => {
+									eliminarTarea(index);
+								}}>
+								Eliminar
+							</button>
+						</h6>
+					</>
+				);
+			})}
+
+		</div>
+	);
+};
+export default Home;
